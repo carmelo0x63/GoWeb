d4c2abf903e23384fa2988316cc51799263be670
Carmelo C <carmelo.califano@gmail.com>
2023-06-15 12:47:31 +0200

    Renamed main.go to goweb.go

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM golang:alpine3.18 AS builder
-COPY main.go src/
-RUN go build -o main src/main.go
+COPY goweb.go src/
+RUN go build -o goweb src/goweb.go
 
 FROM alpine:3.18
 RUN mkdir /app
 WORKDIR /app
-COPY --from=builder /go/main ./
-CMD ["./main"]
+COPY --from=builder /go/goweb ./
+CMD ["./goweb"]
 
--- goweb.go
+++ goweb.go
@@ -8,18 +8,23 @@ import (
 	"strings"
 )
 
+var listenPort = ":8080"
+
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	hostname, _ := os.Hostname()
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
-	message = "This is " + hostname + " running on " + runtime.GOOS + "/" + runtime.GOARCH + " saying: " + message + "\n"
-	fmt.Printf("got / request\n")
+	message = "This is " + hostname +
+                  " running on " + runtime.GOOS +
+                  "/" + runtime.GOARCH + " saying: " +
+                  message + "\n"
+	fmt.Printf("GoWeb: got / request\n")
 	w.Write([]byte(message))
 }
 
 func main() {
 	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(listenPort, nil); err != nil {
 		panic(err)
 	}
 }
